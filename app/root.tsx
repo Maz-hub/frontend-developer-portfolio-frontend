5a7dd198cb2a1e1193196c5737f0a130098c918c
Maz <mazoolander@me.com>
2026-05-20 19:20:11 +0200

    feat: add Google Analytics tracking

--- app/root.tsx
+++ app/root.tsx
@@ -47,6 +47,18 @@ export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-1MNYVQ79SV"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-1MNYVQ79SV');`,
+          }}
+        />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
